ab3461eecb52e8f2f6212e13b0ba6ae19ef6cce2
Jacke <jacke.verbel@gmail.com>
2024-10-15 23:13:44 -0500

    Cambio de clases

--- Taller2/obj/Debug/net8.0/Taller2.AssemblyInfo.cs
+++ Taller2/obj/Debug/net8.0/Taller2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taller2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c32028b9f0a53bb4ed3b85780d837702b853cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e09d7611f05c9aba39a7c8d4123b22b640079f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taller2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taller2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
